c8a92031b82d834706b958b10c37ee350f1da03a
indrikutis <i.aruodziute@lancaster.ac.uk>
2020-10-23 10:54:19 +0100

    Implemented flags.

--- dist/BombSquare.java
+++ dist/BombSquare.java
@@ -1,46 +1,60 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class BombSquare extends GameSquare {
 
     private GameBoard board; // Object reference to the GameBoard this square is part of.
-    private boolean hasBomb; // True if this square contains a bomb. False otherwise.
-    private int visitedCells[][] = new int[30][30]; // Detects which cells were checked.
+    private boolean hasBomb; // State if a square has a bomb or not.
+    private boolean visited; // State if a square was visited or not.
+    private boolean flag; // State if a square has a flag on or not.
 
     public static final int MINE_PROBABILITY = 10;
 
+    /**
+     * Create a new BombSquare, which can be placed on a GameBoard.
+     * 
+     * @param x     the x co-ordinate of this square on the game board.
+     * @param y     the y co-ordinate of this square on the game board.
+     * @param board object reference to the GameBoard this square is part of
+     */
     public BombSquare(int x, int y, GameBoard board) {
         super(x, y, "images/blank.png");
 
         this.board = board;
         this.hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
-
+        this.visited = false;
+        this.flag = false;
     }
 
+    /**
+     * Initiated by the left mouse click. Sets the image of the game square: empty,
+     * bomb, or a number of surrounding bombs in surrounding game squares.
+     */
     @Override
     public void leftClicked() {
-
         if (this.hasBomb) {
             this.setImage("images/bomb.png");
             System.out.println("Game End");
         } else {
-
-            for (int i = 0; i < visitedCells.length; i++) {
-                for (int k = 0; k < visitedCells.length; k++) {
-                    this.visitedCells[i][k] = 0;
-                }
-            }
-
-            recursion(this);
+            displaySquares(this);
         }
     }
 
-    public void recursion(GameSquare square) {
+    /**
+     * 
+     * @param square the square on the game board.
+     */
+    public void displaySquares(GameSquare square) {
         int bombCount = 0;
 
         int x = square.getXLocation();
         int y = square.getYLocation();
-        this.visitedCells[x][y] = 1;
+
+        ((BombSquare) square).setVisited(true);
 
         GameSquare squares[] = new BombSquare[9];
 
+        // Surrounding game squares.
         squares[0] = this.board.getSquareAt(x, y);
         squares[1] = this.board.getSquareAt(x - 1, y - 1);
         squares[2] = this.board.getSquareAt(x - 1, y);
@@ -51,18 +65,20 @@ public class BombSquare extends GameSquare {
         squares[7] = this.board.getSquareAt(x + 1, y);
         squares[8] = this.board.getSquareAt(x + 1, y + 1);
 
+        // Counts the number of surrounding bombs.
         for (int i = 0; i < squares.length; i++) {
-            if (squares[i] != null && ((BombSquare) squares[i]).getHasBomb()) {
+            if (squares[i] != null && ((BombSquare) squares[i]).hasBomb) {
                 bombCount++;
             }
         }
 
+        // Checks the surrounding game squares of an empty game square.
         if (bombCount == 0) {
             square.setImage("images/0.png");
             for (int i = 0; i < squares.length; i++) {
                 if (squares[i] != null) {
-                    if (this.visitedCells[squares[i].getXLocation()][squares[i].getYLocation()] != 1) {
-                        recursion(squares[i]);
+                    if (!((BombSquare) squares[i]).getVisited()) {
+                        displaySquares(squares[i]);
                     }
                 }
             }
@@ -71,12 +87,55 @@ public class BombSquare extends GameSquare {
         }
     }
 
+    /**
+     * Initiated by the right mouse click. Sets the flag on the square by the rules
+     * of the game. Flag can only be set on blank squares.
+     */
     @Override
     public void rightClicked() {
-        this.setImage("images/flag.png");
+
+        boolean visited = this.getVisited();
+        boolean flag = this.getFlag();
+
+        if (!visited && !flag) {
+            setImage("images/flag.png");
+            this.setFlag(true);
+        } else if (!visited && flag) {
+            setImage("images/blank.png");
+            this.setFlag(false);
+        }
     }
 
-    public boolean getHasBomb() {
-        return this.hasBomb;
+    /**
+     * 
+     * @return if the square was visited or not.
+     */
+    public boolean getVisited() {
+        return this.visited;
     }
+
+    /**
+     * 
+     * @param visited sets the state if the square was visited or not.
+     */
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    /**
+     * 
+     * @return if the square has a flag on or not.
+     */
+    public boolean getFlag() {
+        return this.flag;
+    }
+
+    /**
+     * 
+     * @param flag sets the state if the square has a flag on or not.
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
 }
